fdfcaadb067d390395f4e3c3c4174996a6637438
Manveen Kaur <manveenkaur0318@gmail.com>
2022-12-16 15:55:37 +0530

    new app created

---
+++ src/app/recipies/recipies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipies',
+  templateUrl: './recipies.component.html',
+  styleUrls: ['./recipies.component.scss']
+})
+export class RecipiesComponent {
+   
+}
+
